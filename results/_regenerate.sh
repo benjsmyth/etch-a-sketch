4b67935dc30f81e4b3a9440ba4149d50f2fbeb24
Guillaume Isabelle <jgi@jgwill.com>
2021-04-06 12:41:40 -0400

    chg:updated-result

--- results/_regenerate.sh
+++ results/_regenerate.sh
@@ -11,14 +11,31 @@ mvcmd="git mv"
 # copy drop dir pattern in here
 cp $dropdir/$fpattern* .
 
+# Fixing naming
+for f in *png *jpg *jpeg
+do
+        ff=$(echo $f | tr " " "_" | tr "jpeg" "jpg")
+	echo "Processing: $f into "
+	echo "...$ff"
+
+        $mvcmd '"'$f'"' $ff
+	mv '"'$f'"' $ff 
+	#echo "Something happened with : $f"
+	echo "$ff ... done"
+
+done
+echo "-------------------Fixed names and migrating drop in here done----"
+echo "--------------------------------------------------------------------"
+
 for f in *png *jpg *jpeg
 do 
-	ff=$(echo $f | tr " " "_")
-	$mvcmd '"'$f'"' $ff || mv '"'$f'"' $ff || echo "Something happened with : $f"
 
-	echo '![]('$ff')  ' >> README.md
+	echo '![]('$f')  ' >> README.md
 done 
 
+echo "---------INDEX Regenerated----------"
+echo "--------------------------------------"
+echo "--- Commiting and pushing results to repo---"
 git add * 
 git commit . -m chg:updated-result
 git pull
