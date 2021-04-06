929e16bc454751a18ba58ebd24a1dd308cfe9fe6
Guillaume Isabelle <jgi@jgwill.com>
2021-04-06 12:37:37 -0400

    chg:updated-result

--- results/_regenerate.sh
+++ results/_regenerate.sh
@@ -4,9 +4,19 @@
 echo "# Results  " > README.md
 echo "  ">> README.md
 
+dropdir=/home/jgi/Desktop
+fpattern=SmallCreation
+mvcmd="git mv"
+
+# copy drop dir pattern in here
+cp $dropdir/$fpattern* .
+
 for f in *png *jpg *jpeg
 do 
-	echo '![]('$f')  ' >> README.md
+	ff=$(echo $f | tr " " "_")
+	$mvcmd '"'$f'"' $ff || mv '"'$f'"' $ff || echo "Something happened with : $f"
+
+	echo '![]('$ff')  ' >> README.md
 done 
 
 git add * 
