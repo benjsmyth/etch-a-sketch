b9d4b98b50889d9e0e49cc2901c32e20a4835fc9
Jean Guillaume <jgi@jgwill.com>
2021-04-20 01:27:01 -0400

    import+setup: NPM js packaging

---
+++ gia-ast-util/gia-ast-util/README.md
@@ -0,0 +1,30 @@
+# Cross Platform Neural Encement
+
+CLI wrapper for Image Neurally Enhanced using containerized infrastructure
+
+Vision: A Neural Enhancement for images encapsulated into a docker container and wrapped for multiplatform by nodeJS
+
+
+# Run from directory where all your files are
+```sh
+gia-ne fileToEnhance.jpg
+# It will start the docker container in bg to do its work
+```
+# Install
+
+```sh
+npm i gia-ne --g
+docker pull guillaumeai/ne
+```
+
+# Dependencies
+
+* Docker
+
+
+----
+
+# Further research
+
+* Use this project as a model to build Command Wrapper for Containerized infrastructure
+
---
+++ gia-ast-util/gia-ast-util/gia-ast-img2stylize-request.js
@@ -0,0 +1,121 @@
+#!/bin/env node
+'use strict';
+
+
+//@STCGoal an input file is transformed into a base64 request for stylization
+
+
+// node v8.11.3
+
+
+
+
+const Buffer = require('safer-buffer').Buffer;
+const path = require('path');
+const fs = require('fs');
+
+
+var args = process.argv.slice(2);
+
+
+if (args[0] == "--help" || args[0] == "-h" || args[0] == "-help" || args[0] == "--h" || !args[0])
+    console.log(`
+  -------------------------------------
+  ContentImage request Preper
+  by Guillaume D-Isabelle, 2021
+  --------------------------------------
+  -------------HELP----------------------
+  Prep a Request file for AST by creating a target JSON request file with a base64 image tag contentImage.
+
+Synopsis:  
+     ./img2stylizationRequest.js [IMAGE] [OutJSON]
+     
+usage : 
+
+./img2stylizationRequest.js mycontent.jpg myrequest.json
+
+------------------------------
+`);
+
+else // Lets do the work
+{
+    var imgFile = args[0];
+    var target = args[1];
+
+
+    console.log("Reading using v3: " + imgFile);
+
+    //encode_base64_v2(imgFile, target);
+    try {
+        encode_base64_v3_to_JSONRequestFile(imgFile, target);
+        console.log("filje: " + target + " should have been created");
+    } catch (error) {
+        console.log("something went wrong: " );
+        console.log(error);
+    }
+
+    
+}
+
+/**
+ * Encode an image file into base64 JSON request file 
+ * @param  {string} filename
+ * @param  {string} targetJsonFile
+ */
+function encode_base64_v3_to_JSONRequestFile(filename, targetJsonFile) {
+    var base64Raw = fs.readFileSync(filename, 'base64');
+
+    var base64 = base64Raw;
+    var ext = path.extname(filename).replace(".", "");
+    if (ext == "jpg" || ext == "JPG" || ext == "JPEG") ext = "jpeg";
+    if (ext == "pneg" || ext == "PNG" || ext == "Png") ext = "png";
+
+
+    if (base64Raw.indexOf("data:") == -1) //fixing the string
+        base64 = `data:image/${ext};base64,`
+            + base64Raw;
+
+    //console.log(base64);
+    var jsonRequest = new Object();
+    jsonRequest.contentImage = base64;
+    var jsonData = JSON.stringify(jsonRequest);
+
+    fs.writeFileSync(targetJsonFile, jsonData);
+    //console.log("Should have saved :" + targetJsonFile);
+
+}
+/**
+ * @param  {string} filename
+ * @param  {string} targetJsonFile
+ */
+function encode_base64_v2(filename, targetJsonFile) {
+    fs.readFile(filename, function (error, data) {
+        if (error) {
+            throw error;
+        } else {
+            let buf = Buffer.from(data);
+            let base64 = buf.toString('base64');
+
+            var jsonRequest = new Object();
+            jsonRequest.stylizedImage = base64;
+            var jsonData = JSON.stringify(jsonRequest);
+
+            // console.log('Base64 ' + filename + ': ' + base64);
+            fs.writeFileSync(targetJsonFile, jsonData);
+            console.log("Should have saved :" + targetJsonFile);
+
+            return base64;
+        }
+    });
+}
+
+/**
+ * @param  {string} base64str
+ * @param  {string} filename
+ */
+function decode_base64(base64str, filename) {
+    let buf = Buffer.from(base64str, 'base64');
+
+    fs.writeFileSync(filename, buf);
+
+}
