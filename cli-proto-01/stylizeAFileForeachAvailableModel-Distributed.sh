f6e73cd14e1f2f5c61cdb83c7416bbd8c97994ac
JGWill <jgi@jgwill.com>
2021-04-21 10:15:46 -0400

    chg:clean-output

--- cli-proto-01/stylizeAFileForeachAvailableModel-Distributed.sh
+++ cli-proto-01/stylizeAFileForeachAvailableModel-Distributed.sh
@@ -10,18 +10,18 @@ export chost=$callhostB
 for p in $ports
 do
 	#echo "# $p"
-	echo "Stylizing $1 using model on Port ID: $p and host $callhost"
+	#echo "Stylizing $1 using model on Port ID: $p and host $callhost"
 	source $DIR/build.sh $1 $p &
 	if [ "$chost" == "$callhost" ] ;
 	 then
 		#echo "DEBUG::We are already infering with Host $callhost"
 		#Switching host for next iteration
-		echo "Switching to Host B"
+		#echo "Switching to Host B"
 		export callhost=$callhostA
 	else
-		echo "Switching to Host A"
+		#echo "Switching to Host A"
 		export callhost=$callhostB
 	fi
-	echo "sleep $astiaDistributedAverageSwitchTimeframe #Average time for infering with those hosts"
+	##echo "sleep $astiaDistributedAverageSwitchTimeframe #Average time for infering with those hosts"
 	sleep $astiaDistributedAverageSwitchTimeframe #Average time for infering with those hosts
 done
