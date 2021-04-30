a8ae6b1c32e3266b7d1ef0b295ed5aa00d4af0d5
JGWill <jgi@jgwill.com>
2021-04-30 07:24:30 -0400

    feat(cli):NodeJS cli works

--- cli-proto-02-nodejs/x-axios-02.js
+++ cli-proto-02-nodejs/x-axios-02.js
@@ -1,24 +1,27 @@
 #!/bin/env node
 'use strict';
 
+const config = require('./config');
+
+
+
+
+
 const http = require('http');
 const axios = require('axios').default;
-
-var portbase = "90";
-const hostname = 'orko.guillaumeisabelle.com';
+var path = require('path');
 
 const Buffer = require('safer-buffer').Buffer;
-const path = require('path');
 const fs = require('fs');
 
 
 var args = process.argv.slice(2);
 
 
-if (args[0] == "--help" || args[0] == "-h" || args[0] == "-help" || args[0] == "--h" || !args[0])
-console.log(`
+if (args[0] == "--help" || args[0] == "-h" || args[0] == "-help" || args[0] == "--h" || !args[0] || !args[1]) {
+    console.log(`
 -------------------------------------
-AST Web API Stylizer CLI Wropper
+AST Web API Stylizer CLI Wrapper
 by Guillaume D-Isabelle, 2021
 --------------------------------------
 -------------HELP----------------------
@@ -28,37 +31,44 @@ Synopsis:
 ./gia-ast.js [IMAGE] [ModelID]
 
 usage : 
-
 ./gia-ast.js mycontent.jpg 91
 ./gia-ast.js mycontent.jpg 01
 ./gia-ast.js mycontent.jpg 12
 
 ------------------------------
-`);
-
+  `);
+    if (!args[0] || !args[1]) console.log("MISSING ARGUMENTS");
+}
 else // Lets do the work
 {
-  
+
   var stylizedImage;
   var imgFile = args[0];
+  var ext = path.extname(imgFile);
+  var imgFileBasename = path.basename(imgFile);
+  var imgFileNameOnly = imgFileBasename.replace(ext, "");
   var modelid = args[1];
-  var portnum = portbase + modelid;
-  var resultSuffix = "__stylize__" + modelid;
-  
+  var targetOutput = imgFileNameOnly + config.outsuffix + modelid + ext;
+  console.log("TargetOutput: " + targetOutput);
+  var portnum = config.portbase + modelid;
+  const callurl = config.callprotocol + "://" + config.hostname + ":" + portnum + config.callmethod;
+
+  var resultSuffix = config.outsuffix + modelid;
+
   console.log("Processing: " + imgFile + " at port :" + portnum);
 
   //encode_base64_v2(imgFile, target);
   try {
     var data = encode_base64_v3_to_JSONRequestStringify(imgFile);
-    
-        
-          
+
+    //var unparsedData = JSON.parse(data);
+
     //---------------------
     // const data = JSON.stringify({
     //   todo: 'Buy the milk'
     // })
     const options = {
-      hostname: hostname,
+      hostname: config.hostname,
       port: portnum,
       path: '/stylize',
       method: 'POST',
@@ -68,19 +78,39 @@ else // Lets do the work
       }
     };
 
-    console.log("Calling : " + hostname);
+    console.log("Calling : " + config.hostname);
 
-axios
-  .post(hostname,data)
-  .then(res => {
-    console.log(`statusCode: ${res.statusCode}`)
-   // console.log(res)
-    //process.stdout.write(res);
-  })
-  .catch(error => {
-    console.log("There were errors");
-    console.error(error.message);
-  })
+    axios.post(callurl, data, options)
+      .then(function (response) {
+        // var d = JSON.parse(response);
+        var { data } = response;
+        var { stylizedImage } = data;
+
+
+        //---import
+
+        decode_base64_to_file(stylizedImage, targetOutput);
+
+        //console.log(stylizedImage);
+      })
+      .catch(function (err) {
+        console.log("There was error");
+        console.log(err.message);
+      });
+
+
+
+    // axios
+    //   .post(config.hostname,data)
+    //   .then(res => {
+    //     console.log(`statusCode: ${res.statusCode}`)
+    //    // console.log(res)
+    //     //process.stdout.write(res);
+    //   })
+    //   .catch(error => {
+    //     console.log("There were errors");
+    //     console.error(error.message);
+    //   })
 
 
     // const req = http.request(options, res => {
@@ -89,7 +119,7 @@ axios
     //   res.on('data', d => {       
     //     //process.stdout.write(d);
     //   });
-      
+
     //   res.on('end',d =>
     //   {
     //    // console.log(res);
@@ -97,7 +127,7 @@ axios
     //     // jsonDataResult.stylizedImage = "---";
     //     // process.stdout.write(jsonDataResult);
     //   });
-      
+
     // }); 
 
     // req.on('error', error => {
@@ -108,15 +138,15 @@ axios
     // req.end()
 
 
-//-----------------------
+    //-----------------------
 
 
   } catch (error) {
-      console.log("something went wrong: " );
-      console.log(error);
+    console.log("something went wrong: ");
+    console.log(error);
   }
 
-  
+
 }
 
 
@@ -130,7 +160,7 @@ axios
  * @param  {string} filename
  * @param  {string} targetJsonFile
  */
- function encode_base64_v3_to_JSONRequestStringify(filename) {
+function encode_base64_v3_to_JSONRequestStringify(filename) {
   var base64Raw = fs.readFileSync(filename, 'base64');
 
   var base64 = base64Raw;
@@ -140,8 +170,8 @@ axios
 
 
   if (base64Raw.indexOf("data:") == -1) //fixing the string
-      base64 = `data:image/${ext};base64,`
-          + base64Raw;
+    base64 = `data:image/${ext};base64,`
+      + base64Raw;
 
   //console.log(base64);
   var jsonRequest = new Object();
@@ -149,7 +179,7 @@ axios
   var jsonData = JSON.stringify(jsonRequest);
 
   return jsonData;
-//  fs.writeFileSync(targetJsonFile, jsonData);
+  //  fs.writeFileSync(targetJsonFile, jsonData);
   //console.log("Should have saved :" + targetJsonFile);
 
 }
@@ -171,8 +201,8 @@ function encode_base64_v3_to_JSONRequestFile(filename, targetJsonFile) {
 
 
   if (base64Raw.indexOf("data:") == -1) //fixing the string
-      base64 = `data:image/${ext};base64,`
-          + base64Raw;
+    base64 = `data:image/${ext};base64,`
+      + base64Raw;
 
   //console.log(base64);
   var jsonRequest = new Object();
@@ -189,22 +219,22 @@ function encode_base64_v3_to_JSONRequestFile(filename, targetJsonFile) {
 */
 function encode_base64_v2(filename, targetJsonFile) {
   fs.readFile(filename, function (error, data) {
-      if (error) {
-          throw error;
-      } else {
-          let buf = Buffer.from(data);
-          let base64 = buf.toString('base64');
+    if (error) {
+      throw error;
+    } else {
+      let buf = Buffer.from(data);
+      let base64 = buf.toString('base64');
 
-          var jsonRequest = new Object();
-          jsonRequest.stylizedImage = base64;
-          var jsonData = JSON.stringify(jsonRequest);
+      var jsonRequest = new Object();
+      jsonRequest.stylizedImage = base64;
+      var jsonData = JSON.stringify(jsonRequest);
 
-          // console.log('Base64 ' + filename + ': ' + base64);
-          fs.writeFileSync(targetJsonFile, jsonData);
-          console.log("Should have saved :" + targetJsonFile);
+      // console.log('Base64 ' + filename + ': ' + base64);
+      fs.writeFileSync(targetJsonFile, jsonData);
+      console.log("Should have saved :" + targetJsonFile);
 
-          return base64;
-      }
+      return base64;
+    }
   });
 }
 
@@ -219,3 +249,27 @@ function decode_base64(base64str, filename) {
 
 }
 
+
+
+//---import from gia-ast-util-responseStylize-to-file
+
+
+/**
+ * @param  {string} base64str
+ * @param  {string} filename
+ */
+function decode_base64_to_file(base64str, filename) {
+
+  if (base64str.indexOf("png;base64") > -1)
+    base64str = base64str
+      .replace(/^data:image\/png;base64,/, "");
+
+  if (base64str.indexOf("jpeg;base64") > -1) {
+    base64str = base64str
+      .replace(/^data:image\/jpeg;base64,/, "");
+  }
+  let buf = Buffer.from(base64str, 'base64');
+
+  fs.writeFileSync(filename, buf);
+
+}
