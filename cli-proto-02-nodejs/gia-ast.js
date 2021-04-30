5ce39c7ab50bb1d2ac492cfb8cdc99404433e4e9
JGWill <jgi@jgwill.com>
2021-04-30 09:18:44 -0400

    ver

--- cli-proto-02-nodejs/gia-ast.js
+++ cli-proto-02-nodejs/gia-ast.js
@@ -53,6 +53,7 @@ if (args[0] == "--help" || args[0] == "-h" || args[0] == "-help" || args[0] == "
 -------------------------------------
 AST Web API Stylizer CLI Wrapper
 by Guillaume D-Isabelle, 2021
+Version 0.1.2
 --------------------------------------
 -------------HELP----------------------
 Stylize an image using the Web API.
