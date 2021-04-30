f0aa78fe2621ef0a0c6e10a48c206984c69e4a46
JGWill <jgi@jgwill.com>
2021-04-30 10:04:31 -0400

    fix

--- cli-proto-02-nodejs/prepack.js
+++ cli-proto-02-nodejs/prepack.js
@@ -5,8 +5,8 @@ var json = fs.readFileSync("package.json");
 var data = JSON.parse(json);
 var mainScript = "gia-ast.js";
 fs.copyFileSync(mainScript,mainScript+".prepublish");
-var script = "---";
-script = fs.readFileSync(mainScript);
+var script = 'String';
+script = String(fs.readFileSync(mainScript));
 
 fs.writeFileSync(mainScript,script.replace("VERSIONFLAG",data.version));
 
