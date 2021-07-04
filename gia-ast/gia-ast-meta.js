077a14d24b9ba0a1e10b69b6be1bac621a51ca2b
JGWill-Orko <jgi@jgwill.com>
2021-07-04 05:39:38 -0400

    upgrade:version

--- gia-ast/gia-ast-meta.js
+++ gia-ast/gia-ast-meta.js
@@ -28,7 +28,7 @@ if (args[0] == "--help" || args[0] == "-h" || args[0] == "-help" || args[0] == "
 -------------------------------------
 AST Web API Stylizer CLI Wrapper
 by Guillaume D-Isabelle, 2021
-Version 0.2.4
+Version 0.3.4
 --------------------------------------
 -------------HELP----------------------
 Stylize an image using the Web API.
